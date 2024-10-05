0d6fdb87cb2c13c02119c312add3bbaa47e823e8
acocoles <acocoles@gmail.com>
2024-10-05 14:50:34 -0700

    Test Map and Gnat started
    Made test map with initial shapes but no textures. Gnat is moving and flying around within a confined space.

---
+++ itsbitsy/Bugs/Gnats/gnat.gd
@@ -0,0 +1,42 @@
+extends Node2D
+
+@export var speed = 30 #pixels/sec
+@export var wanderStep = 45 #distance between each wander
+@export var wanderMax = 40 #furthest from spawn point to wander
+
+var targetPosition = Vector2.ZERO
+var velocity = Vector2.ZERO
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	$WaitTimer.start()
+	
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	move(delta)	
+	queue_redraw()
+
+func _draw() -> void:
+	draw_circle($InitialSpawnPoint.position, 3, Color.YELLOW)
+	draw_circle(targetPosition, 3, Color.DARK_RED)
+	
+func move(delta: float):
+	#--- MOVEMENT ---
+	# move towards target position
+	if ($WaitTimer.time_left == 0) : velocity = $GnatBody.position.direction_to(targetPosition) * speed
+	# if near target postion, wait
+	if ($GnatBody.position.distance_to(targetPosition) < (speed * 0.1) and $WaitTimer.time_left == 0):
+		$WaitTimer.start()
+		velocity = Vector2.ZERO
+	
+	$GnatBody.position += velocity * delta
+	# repeat
+	
+func pickPoint():
+	targetPosition = $GnatBody.position + Vector2(randi_range(-wanderStep,wanderStep),randi_range(-wanderStep,wanderStep))
+	while (targetPosition.distance_to($InitialSpawnPoint.position) > wanderMax): targetPosition = $GnatBody.position + Vector2(randi_range(-wanderStep,wanderStep),randi_range(-wanderStep,wanderStep))
+	targetPosition = targetPosition.round()
+	
+func _on_wait_timer_timeout() -> void:
+	pickPoint()
