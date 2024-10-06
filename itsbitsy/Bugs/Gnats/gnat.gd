c1800c044d355f4f7979241741a2b59e5ba185ae
acocoles <acocoles@gmail.com>
2024-10-05 23:23:19 -0700

    Captured Anim added

--- itsbitsy/Bugs/Gnats/gnat.gd
+++ itsbitsy/Bugs/Gnats/gnat.gd
@@ -22,6 +22,7 @@ func _process(delta: float) -> void:
 func test() :
 	print("child")
 	super.test()
+	
 #func _draw() -> void:
 	#draw_circle($InitialSpawnPoint.position, wanderMax, Color.SKY_BLUE)
 	#draw_circle($InitialSpawnPoint.position, 3, Color.YELLOW)
