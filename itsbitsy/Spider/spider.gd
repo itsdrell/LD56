41930a8ea03c0aef58f0e44ca0092c439a190797
Drell <ztbracken@gmail.com>
2024-10-05 17:33:26 -0700

    spider pendulum

--- itsbitsy/Spider/spider.gd
+++ itsbitsy/Spider/spider.gd
@@ -6,15 +6,72 @@ var edgeOfSprite
 var nextToSpider
 var DesiredPosition
 
+var inWebMode = false;
+var StartingWebAnchorPoint;
+
+var angle = PI / 4
+var angleV = 0 
+var angleA = 0
+@export var WebLength = 16
+@export var SwingGravity = .1
+@export var SwingInputForce = 5
+@export var AddedWebLength = 32
+@export var WebScene : PackedScene
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	DesiredPosition = position
 
 func _draw() -> void:
 	#draw_circle($Check.position, 64, Color.YELLOW)
-	pass
+
+	if inWebMode:
+		draw_line(to_local(StartingWebAnchorPoint), to_local(position), Color.WHITE, 6)
+
 
 func _process(delta):	
+	
+	if !inWebMode:
+		UpdateNormalMovement(delta);
+	else:
+		WebModeUpdate(delta);
+	
+	queue_redraw()
+	
+func WebModeUpdate(delta):
+	var desired_velocity = Vector2.ZERO # The player's movement vector.
+	if Input.is_action_pressed("move_down"):
+		WebLength += AddedWebLength * delta
+		
+	if Input.is_action_just_released("move_left"):
+		angleV -= SwingInputForce * delta
+	if Input.is_action_just_released("move_right"):
+		angleV += SwingInputForce * delta
+	#if Input.is_action_just_released("interact"):
+		#var newWeb = WebScene.instantiate()
+		#newWeb.add_point(StartingWebAnchorPoint)
+		#newWeb.add_point(position)
+		#newWeb.position = StartingWebAnchorPoint
+		#add_child(newWeb)
+
+	var force = SwingGravity * sin(angle)
+	angleA = (-1 * force) / WebLength
+	angleV += angleA
+	angle += angleV
+	
+	var xPos = WebLength * sin(angle) + StartingWebAnchorPoint.x
+	var yPos = WebLength * cos(angle) + StartingWebAnchorPoint.y
+	
+	position = Vector2(xPos, yPos);
+	
+	angleV *= .99
+	
+func UpdateNormalMovement(delta):
+	
+	if Input.is_action_pressed("interact"):
+		inWebMode = true 
+		StartingWebAnchorPoint = position
+		return
 	
 	var new_velocity = CalculateVelocity();
 	if(new_velocity == Vector2.ZERO):
@@ -23,8 +80,6 @@ func _process(delta):
 	DesiredPosition = position + (new_velocity * delta * speed)
 	
 	$Check.position = $Check.to_local(DesiredPosition);
-
-	queue_redraw()
 	
 func CalculateVelocity() -> Vector2:
 	var desired_velocity = Vector2.ZERO # The player's movement vector.
@@ -40,6 +95,9 @@ func CalculateVelocity() -> Vector2:
 	return desired_velocity.normalized()
 
 func _physics_process(delta):
+	if inWebMode : 
+		return
+	
 	var checkOverlaps = $Check.get_overlapping_bodies();
 	var isValidMoveLocation = !checkOverlaps.is_empty()
 	
